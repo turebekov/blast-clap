1b1d55554fab036f2c95beea94bedb913791ee82
Turebekov Arman <turebekov.arman@gitlab.com>
2024-07-28 10:03:22 +0500

    Create project with webpack config

---
+++ webpack.loc.js
@@ -0,0 +1,54 @@
+const webpack = require("webpack")
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
+
+module.exports = {
+    mode: "development",
+    devtool: "eval-cheap-source-map",
+    output: {
+        clean: true, // Clean the output directory before emit
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+                test: [/\.vert$/, /\.frag$/],
+                use: "raw-loader"
+            },
+            {
+                test: /\.(png|jpe?g|atlas)$/i,
+                use: [
+                    {
+                        loader: "file-loader",
+                    },
+                ],
+            },
+        ]
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { // не копируем изображения в папках
+                    from: "src/assets/images/*",
+                    to: "assets/images/[name][ext]"
+                },
+            ],
+        }),
+        new webpack.DefinePlugin({
+            CANVAS_RENDERER: JSON.stringify(true),
+            WEBGL_RENDERER: JSON.stringify(true)
+        }),
+        new HtmlWebpackPlugin({
+            template: "./index.html"
+        })
+    ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000,
+        open: true,
+        hot: true,
+        watchFiles: ['src/**/*'] // Наблюдение за изменениями в src
+    }
+}
